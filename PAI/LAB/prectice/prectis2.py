3bf1f4f99abe505dcb16ab7e267db0ec24853b50
talharusman <142867808+talharusman@users.noreply.github.com>
2024-09-01 10:26:31 +0500

    Delete PAI/LAB/prectice/prectis2.py

--- PAI/LAB/prectice/prectis2.py
+++
@@ -1,6 +0,0 @@
-def calclat_Area(lenght,width):
-    return lenght*width
-length = int(input("Enter the lenght:"))
-width = int(input("enter the width"))
-area = calclat_Area(length,width)
-print(f"The are of rectangl is {area}")
