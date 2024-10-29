aefd72a4589a5177c57835eee57628f8d590ffb0
talharusman <142867808+talharusman@users.noreply.github.com>
2024-10-29 22:52:11 +0500

    Delete Prectice2.py

--- Prectice2.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-import pandas as pd
-
-df = pd.read_csv("Iris.csv")
-x = df["Species"]
-y = df["SepalLengthCm"]
-clr = ['g', 'm', 'y', 'b']
-plt.bar(x, y, color=clr)
-plt.show()
